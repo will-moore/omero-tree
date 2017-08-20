9edbf38f2d2c61bb13e8b720144aea1667d27693
William Moore <w.moore@dundee.ac.uk>
2017-08-20 22:28:03 +0100

    Tree component

--- src/App.js
+++ src/App.js
@@ -1,98 +1,17 @@
-import cx from 'classnames';
 import React, { Component } from 'react';
-import Tree from 'react-ui-tree';
-import { fetchObjects } from './actions';
-import { connect } from 'react-redux';
+import Tree from './Tree';
 
 class App extends Component {
-  componentWillMount = () => {
-    this.props.fetchObjects('projects');
-  };
-
-  renderNode = node => {
-    return (
-      <span
-        className={cx('node', {
-          'is-active': node === this.props.active
-        })}
-        onClick={this.onClickNode.bind(null, node)}
-      >
-        {node.name} ({node.childCount})
-      </span>
-    );
-  };
-
-  onClickNode = node => {
-    if (node.childCount > 0 && node.children[0].name === 'loading...') {
-      console.log('load...');
-      this.props.fetchObjects('datasets', node.id);
-    }
-    this.setState({
-      active: node
-    });
-  };
-
   render() {
     return (
-      <div className="app">
+      <div>
+        <h1>OMERO</h1>
         <div>
-          {this.props.active ? this.props.active.name : 'NONE'}
-        </div>
-        <div className="tree">
-          <Tree
-            paddingLeft={20}
-            tree={this.props.tree}
-            onChange={this.handleChange}
-            isNodeCollapsed={this.isNodeCollapsed}
-            renderNode={this.renderNode}
-          />
-        </div>
-        <div className="inspector">
-          <h1>Test....</h1>
-          <button onClick={this.updateTree}>update tree</button>
-          <pre>
-            {JSON.stringify(this.props.tree, null, '  ')}
-          </pre>
+          <Tree />
         </div>
       </div>
     );
   }
-
-  handleChange = tree => {
-    // this.setState({
-    //   tree: tree
-    // });
-  };
-
-  updateTree = () => {
-    // const { tree } = this.state;
-    // tree.children.push({ name: 'test' });
-    // this.setState({
-    //   tree: tree
-    // });
-  };
 }
 
-// Define how state from store gets mapped to
-// props of child component <ChannelList>
-const mapStateToProps = (state, ownProps) => {
-  return {
-    tree: state.tree,
-    active: state.active
-  };
-};
-
-// Define functions that modify store and are
-// passed as props of App so that it
-// can update store
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    fetchObjects: (dtype, parentId) => {
-      dispatch(fetchObjects(dtype, parentId));
-    }
-  };
-};
-
-const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
-
-export default AppContainer;
+export default App;
